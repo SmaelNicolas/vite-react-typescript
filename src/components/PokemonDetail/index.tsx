3af3c74a3844a2094f10cc8cfb5fa0de0bfdf6de
Smael Nicolas <nico.smael@gmail.com>
2022-11-25 14:47:43 -0300

    fix error return ;

--- src/components/PokemonDetail/index.tsx
+++ src/components/PokemonDetail/index.tsx
@@ -14,38 +14,45 @@ interface Props {
 }
 
 export const PokemonDetail = ({ pokemon }: Props) => {
+	/* @ts-ignore */
+	const backgroundSelected = background[pokemon?.types[0]?.type?.name];
 
-    /* @ts-ignore */
-    const backgroundSelected = background[pokemon?types[0]?.type?.name]
+	if (!pokemon) {
+		return (
+			<div
+				style={{ background: backgroundSelected }}
+				className={styles.loadingContainer}>
+				<Loader size={50} color='fff' />
+			</div>
+		);
+	}
 
-
-    if (!pokemon){
-        return(
-            <div
-                style={{background: backgroundSelected}}
-                className={styles.loadingContainer}
-            >
-            <Loader size={50} color="fff" />
-            </div>
-        )
-    }
-    
-	 return (
-    <div style={{ background: backgroundSelected }} className={styles.bg}>
-      <Header pokemon={pokemon} />
-      <div className={styles.info}>
-        <img
-          src={
-            pokemon?.sprites?.other?.dream_world?.front_default ||
-            pokemon?.sprites?.front_default
-          }
-          alt={pokemon?.name}
-        />
-        <PokeTypes pokemon={pokemon} />
-        <Title content="About" backgroundSelected={backgroundSelected} />
-        <Stats pokemon={pokemon} />
-        <Title content="Base Stats" backgroundSelected={backgroundSelected} />
-        <BaseStats pokemon={pokemon} backgroundSelected={backgroundSelected} />
-      </div>
-    </div>
+	return (
+		<div style={{ background: backgroundSelected }} className={styles.bg}>
+			<Header pokemon={pokemon} />
+			<div className={styles.info}>
+				<img
+					src={
+						pokemon?.sprites?.other?.dream_world?.front_default ||
+						pokemon?.sprites?.front_default
+					}
+					alt={pokemon?.name}
+				/>
+				<PokeTypes pokemon={pokemon} />
+				<Title
+					content='About'
+					backgroundSelected={backgroundSelected}
+				/>
+				<Stats pokemon={pokemon} />
+				<Title
+					content='Base Stats'
+					backgroundSelected={backgroundSelected}
+				/>
+				<BaseStats
+					pokemon={pokemon}
+					backgroundSelected={backgroundSelected}
+				/>
+			</div>
+		</div>
+	);
 };
