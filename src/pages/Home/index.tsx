1a3d22dc4025babf25511d7d89c78ceebbee381b
Smael Nicolas <nico.smael@gmail.com>
2022-11-25 14:12:25 -0300

    import custom hook , pass variables as props

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,16 @@
 import { useContext } from "react";
 import { PokeballIconSmall } from "../../assets/pokeball";
+import { Pagination } from "../../components/Pagination";
 import { PokemonList } from "../../components/PokemonList";
 import { PokemonContext } from "../../context/PokemonContext";
+import { usePagination } from "../../hooks/usePagination";
 import styles from "./styles.module.scss";
 
 export const Home = () => {
 	const { pokemonsFiltered } = useContext(PokemonContext);
+	const { page, nextPage, previousPage, backToHome } = usePagination();
+
+	let perPage = 12;
 
 	return (
 		<div className={styles.home}>
@@ -15,7 +20,18 @@ export const Home = () => {
 					<span>Pokedex</span>
 				</div>
 			</header>
-			<PokemonList pokemonsUrls={pokemonsFiltered} />
+			<PokemonList
+				page={page}
+				perPage={perPage}
+				pokemonsUrls={pokemonsFiltered}
+			/>
+			<Pagination
+				page={page}
+				perPage={perPage}
+				nextPage={nextPage}
+				previousPage={previousPage}
+				maxItems={pokemonsFiltered?.length!}
+			/>
 		</div>
 	);
 };
